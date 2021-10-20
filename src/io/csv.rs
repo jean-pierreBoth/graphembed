0089f20f0fae457eec8e5535d31c6cd88c9b53a9
Jean-Pierre Both <jeanpierre.both@gmail.com>
2021-10-20 21:14:30 +0200

    begins csv reading

--- src/io/csv.rs
+++ src/io/csv.rs
@@ -1,14 +1,23 @@
 //! Construct or dump a (small) graph from data from a csv
 
-use csv;
 
 use std::fmt::{Debug};
 
-use std::path::{Path, PathBuf};
-use std::fs::{OpenOptions, File};
+use log::*;
+use anyhow::{anyhow, Error};
+
+
+use std::fs::OpenOptions;
+use std::path::{Path};
+
 use std::io::{Write,BufWriter, BufReader};
 
-use petgraph::graph::*;
+use csv::ReaderBuilder;
+
+use petgraph::graph::{Graph, IndexType};
+use petgraph::{Directed, Undirected, EdgeType};
+
+
 
 // From csv file we have nodes indexed by u32, edge multiplicity as f32,  edge can be negative 
 // (see bitcoin files  https://snap.stanford.edu/data/index.html)
@@ -22,6 +31,57 @@ use petgraph::graph::*;
 /// N and E in Grap<N,E,Ty,Ix are data associated to node and edge respectively>
 /// instantiate with UnDirected for undirected graph
 /// 
-pub fn from_csv<Ty, Ix>(filename : &str) -> Option<Graph<Ty,Ix> > {
-    None
+pub fn directed_from_csv<N, E,Ix>(filepath : &Path) -> anyhow::Result<usize> 
+    where Ix : IndexType {
+    //
+    // initialize a reader from filename, skip lines beginning with # or %
+    let fileres = OpenOptions::new().read(true).open(&filepath);
+    if fileres.is_err() {
+        log::error!("ProcessingState reload_json : reload could not open file {:?}", filepath.as_os_str());
+        println!("ProcessingState reload_json: could not open file {:?}", filepath.as_os_str());
+        return Err(anyhow!("ProcessingState reload_json could not open file"));            
+    }
+    let file = fileres.unwrap();
+    let mut rdr = ReaderBuilder::new().flexible(false).from_reader(file);
+    //
+    let graph = Graph::<N,E, Directed, Ix>::with_capacity(10_000, 100_000);
+    //
+    let mut nb_record = 0;
+    for result in rdr.records() {
+        let record = result?;
+        nb_record += 1;
+        if log::log_enabled!(Level::Info) {
+            log::debug!("{:?}", record);
+        }
+
+    }
+    Ok(1)
 } // end of from_csv
+
+
+#[cfg(test)]
+
+mod tests {
+
+fn log_init_test() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
+#[test]
+fn load_undirected() {
+    // 
+    println!("\n\n load_undirected");
+    log_init_test();
+
+} // end test load_undirected
+
+#[test]
+fn load_directed_unweighted() {
+    // We load CA-GrQc.txt taken from Snap data directory
+    println!("\n\n load_directed");
+    log_init_test();
+
+} // end test load_undirected
+
+
+}  // edn of mod tests
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 //! lib target
 
+
+use env_logger::{Builder};
+
+#[macro_use]
+extern crate  lazy_static;
+
+lazy_static! {
+    static ref LOG: u64 = {
+        let res = init_log();
+        res
+    };
+}
+
+// install a logger facility
+fn init_log() -> u64 {
+    Builder::from_default_env().init();
+    println!("\n ************** initializing logger *****************\n");    
+    return 1;
+}
+
 pub mod io;
