2228d8b1056c512a3d69d8fca0332cb2d96ebd7a
Jean-Pierre Both <jeanpierre.both@gmail.com>
2021-12-06 14:46:28 +0100

    spectral radius estimate oK

--- src/atp/hope.rs
+++ src/atp/hope.rs
@@ -7,7 +7,7 @@
 
 
 
-use ndarray::{Dim, Array1, Array2};
+use ndarray::{Array1, Array2};
 
 use ndarray_linalg::{Scalar, Lapack};
 
@@ -59,11 +59,12 @@ fn estimate_spectral_radius_csmat<F>(mat : &CsMat<F>) -> f64
     let dims = mat.shape();
     let init = F::from_f64(1./(dims.0 as f64).sqrt()).unwrap();
     let mut v1 = Array1::<F>::from_elem(dims.0, init);
-    let mut v2: Array1::<F> = v1.clone();
+    let mut v2 = Array1::<F>::from_elem(dims.0, F::zero());
     let mut iter = 0;
     let mut radius: F;
     let epsil = F::from_f64(1.0E-5).unwrap();
     loop {
+        v2.fill(F::zero());
         let v2_slice = v2.as_slice_mut().unwrap();
         prod::mul_acc_mat_vec_csr(mat.view(), v1.as_slice().unwrap(), v2_slice);
         radius = Scalar::sqrt(v2.dot(&v2));
@@ -71,8 +72,8 @@ fn estimate_spectral_radius_csmat<F>(mat : &CsMat<F>) -> f64
         let w = &v1 - &v2;
         let delta = Scalar::sqrt(w.dot(&w));
         iter += 1;
-        if iter >= 100 || delta < epsil {
-            log::debug!(" estimated radius at iter {} {}", iter, radius.to_f64().unwrap());
+        if iter >= 1000 || delta < epsil {
+            log::info!(" estimated radius at iter {} {}", iter, radius.to_f64().unwrap());
             break;
         }
         v1.assign(&v2);
@@ -95,11 +96,11 @@ fn estimate_spectral_radius_fullmat<F>(mat : &Array2<F>) -> f64
         v2 = mat.dot(&v1);
         radius = Scalar::sqrt(v2.dot(&v2));
         v2 = v2 * F::one()/ radius;
-        let w = v1 - &v2;
+        let w = &v1 - &v2;
         let delta = Scalar::sqrt(w.dot(&w));
         iter += 1;
-        if iter >= 100 || delta < epsil {
-            log::debug!(" estimated radius at iter {} {}", iter, radius.to_f64().unwrap());
+        if iter >= 1000 || delta < epsil {
+            log::info!(" estimated radius at iter {} {}", iter, radius.to_f64().unwrap());
             break;
         }
         v1 = v2;
@@ -114,8 +115,11 @@ fn estimate_spectral_radius_fullmat<F>(mat : &Array2<F>) -> f64
 
 mod tests {
 
+    #[allow(unused)]
 use super::*;
 
+#[allow(unused)]
+use sprs::{CsMat, TriMatBase};
 
 #[allow(dead_code)]
 fn log_init_test() {
@@ -136,6 +140,58 @@ fn test_spectral_radius_full() {
     //
     assert!((radius-10.).abs() < 1.0E-4);
 } // end of test_spectral_radius_full
+
+
+#[test]
+fn test_spectral_radius_csr() {
+    log_init_test();
+    log::info!("in test_spectral_radius_csr"); 
+    // compute radius from a full matrix
+    let mat = ndarray::arr2(&[ [9., 0. , 2.],
+        [0., 8., 4.],
+        [1., 1., 0.]  ]);
+        
+    // check we get the same radius from the same matrix givn as a csr
+    let mut rows = Vec::<usize>::with_capacity(6);
+    let mut cols = Vec::<usize>::with_capacity(6);
+    let mut values = Vec::<f64>::with_capacity(6);
+    // row 0
+    rows.push(0);
+    cols.push(0);
+    values.push(9.);
+
+    rows.push(0);
+    cols.push(2);
+    values.push(2.);
+    // row 1    
+    rows.push(1);
+    cols.push(1);
+    values.push(8.);
+
+    rows.push(1);
+    cols.push(2);
+    values.push(4.); 
+    // row 2
+    rows.push(2);
+    cols.push(0);
+    values.push(1.);
+
+    rows.push(2);
+    cols.push(1);
+    values.push(1.);
+    //
+    let trimat = TriMatBase::<Vec<usize>, Vec<f64>>::from_triplets((3,3),rows, cols, values);
+    let csr_mat : CsMat<f64> = trimat.to_csr();
+    //
+    let radius_from_full = estimate_spectral_radius_fullmat(&mat);
+    log::info!("estimate_spectral_radius_fullmat radius : {}", radius_from_full);
+    let radius_from_csmat = estimate_spectral_radius_csmat(&csr_mat);
+    log::info!("estimate_spectral_radius_csmat radius : {}", radius_from_csmat);
+    //
+    assert!((radius_from_full-radius_from_csmat).abs() < 0.0001 * radius_from_full);
+
+
+}  // enf of test_spectral_radius_csr
 
 
 
