c41884bb642e5df2248a0a8e94791379c88f10af
Jean-Pierre Both <jeanpierre.both@gmail.com>
2022-02-11 12:03:01 +0100

    delared modules validation and gkernel

---
+++ src/gkernel/mod.rs
@@ -0,0 +1,43 @@
+//! This module is dedicated to graph kernels implementations
+//! 
+//! To be implmeented 
+//!     - The base 1-Weisfeiler-Lehman rooted subtree algorithm 
+//!     - A Hashed version to get a continuous attribute  WL version
+//!
+//!  
+//! Some References on Graph Kernels are :  
+//! 
+//! - Graph Kernels : A survey. Nikolentzos-Siglidis-Vazirgiannis 2021
+//!     panorama of graphs kernels. links with  GNN. Examples et perfs GNN et Graph Kernels comparées.
+//!     Core Weisfeiler-Lehman and Vertex Histogram excellent sur unlabeled  or discrete labeled node graph
+//!     Multiscale laplacian is OK for continuous labeled vertex graph but more cpu intensive
+//! 
+//! - Graph Kernels : State of the art and futures challenges 2020.
+//!
+//!  
+// The first paper : 
+// - Shervashidze-Borgwardt Weisfeiler-Lehman Graph Kernels 2011
+//         provides a framework for kernels on unlabeled and discrete labels.
+//         sorting neighbours labels+ compression (hash) and h iterations.
+//         complexity h* nb edges
+//         provides a feature vector for each node, and the whole graph
+//         local and global WL!
+// 
+// - Power Iterated Color Refinement  Kersting-Grohe 2014
+//         Establishes a link between power iterated color algorithm and relaxed matricial optimization between adjacency matrices. 
+//         Systeme de Hash avec nombre premiers!
+//  
+// - Faster Kernels for Graphs with continuous Attributes via Hashing. 2016
+// - Weisfeiler and Lehman go sparse Morris Rattan Mutzel 2020
+// 
+// - Graph invariant Kernels Orsini IJCAI 2015   
+//     definit un kernel pour les attributs. Le kernel global est le kernel sur les attributs * poids 
+//     dependant du kernel sur les sommets et d'une fonction d appariement des structures matchées sous graphes.
+//     gere les attributs continus et montre l amelioration des perf avec que sans.
+//     WL meilleur kernel sur les sommets. Kernel sur les patterns , lables discrets, à batir avec du hash (par 3.1)
+// 
+// 
+// Comparison GNN with KGraph
+// 
+//  - How powerful are Graph Neural Networks Xu-Hu Leskovec 2019
+//      GNN <= Weisfeiler-Lehman test
---
+++ src/validation/mod.rs
@@ -0,0 +1,10 @@
+//! This module implements some validation tools
+//! - link prediction based  on Adamic-Adar, Resource Allocator,  and Local Path Index
+
+/// references
+/// - The Link-Prediction Problem for Social Networks Liben-Nowell Kleinberg 
+///           2007 Journal AMS for Info. Science and Technology
+/// - A Survey of Link Prediction in Complex Networks for Complex Networks
+///          Martinez, Berzal, Cubero 2016
+/// - Path‑based extensions of local link prediction methods for complex networks
+///     Aziz, Gul Nature Scientific reports 2020.
