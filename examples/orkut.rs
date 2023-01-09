1803758a54d99948d8dad4cab1f04d0c07563f1e
Jean-Pierre Both <jeanpierre.both@gmail.com>
2023-01-09 15:00:31 +0100

    display length of communities in notebook

--- examples/orkut.rs
+++ examples/orkut.rs
@@ -1,9 +1,16 @@
 //! example of density decompositon for Orkut graph [https://snap.stanford.edu/data/com-Orkut.html]
 //! The graph is undirected with 3072441 nodes and 117 185 083 edges
 //! The output is the ordered list of densest stable sets
+//! 
+//! Frank-Wolfe 500 iterations run in 12.3mn. The whole computation need 13.4mn on a 8 cores 2-thread/core laptop with i5 intel proc
 
+use anyhow::{anyhow};
 
 use std::path::{Path};
+use std::io::{BufReader};
+use std::fs::{OpenOptions};
+use std::io::prelude::*;
+use std::str::FromStr;
 
 use petgraph::prelude::*;
 
@@ -16,10 +23,10 @@ const ORKUT_DATA_DIR : &'static str = "/home/jpboth/Data/Graphs/Orkut/";
 
 
 /// Read graph (given as csv) and ground truth communities
-pub fn read_orkutdir(dirpath : &Path) -> Result<Graph<u32, f64 , Undirected, u32>, anyhow::Error> {
+fn read_orkut_graph(dirpath : &Path) -> Result<Graph<u32, f64 , Undirected, u32>, anyhow::Error> {
     let fpath = dirpath.clone().join("com-orkut.ungraph.txt");
     // use csv to unweighted graph_map
-    log::info!("read_orkutdir : reading {fpath:?}");
+    log::info!("read_orkut_graph : reading {fpath:?}");
     let graphmap = weighted_csv_to_graphmap::<u32 ,f64, Undirected>(&fpath, b'\t');
     log::info!("read_orkutdir : reading {fpath:?}, done");
     if graphmap.is_err() {
@@ -28,14 +35,49 @@ pub fn read_orkutdir(dirpath : &Path) -> Result<Graph<u32, f64 , Undirected, u32
     let graph = graphmap.unwrap().into_graph::<u32>();
     log::info!("graph loaded");
     Ok(graph)
-} // end of read_orkutdir
+} // end of read_orkutgraph
+
+
+
+fn read_orkut_com(dirpath : &Path) -> anyhow::Result<Vec<Vec<usize>>> {
+    let fpath = dirpath.clone().join("com-orkut.top5000.cmty.txt");
+    log::info!("read_orkut_com : reading {fpath:?}");
+    let fileres = OpenOptions::new().read(true).open(&fpath);
+    if fileres.is_err() {
+        log::error!("read_orkut_com : reload could not open file {:?}", fpath.as_os_str());
+        println!("read_orkut_com could not open file {:?}", fpath.as_os_str());
+        return Err(anyhow!("read_orkut_com could not open file {}", fpath.display()));            
+    }
+    let file = fileres?;
+    let bufreader = BufReader::new(file);
+    let lines = bufreader.lines();
+    let mut numline = 0;
+    let mut communities = Vec::<Vec<usize>>::with_capacity(5000);
+    for line in lines {
+        if line.is_err() {
+            log::error!("error reading file : {:?} at line : {}",fpath.as_os_str(),numline);
+            return Err(anyhow!(" error reading file : {:?} at line : {}",fpath.as_os_str(),numline));
+        }
+        // split and decode line. line consists in usize separated by a tab
+        let line = line.unwrap();
+        let splitted : Vec<&str>= line.split('\t').collect();
+        let communitiy : Vec<usize> = splitted.iter().map(|s| usize::from_str(*s).unwrap()).collect();
+        communities.push(communitiy);
+        numline += 1;
+    }
+    //
+    return Ok(communities);
+} // end of read_orkut_com
+
+
+
 
 
 pub fn main() {
     //
     let _ = env_logger::Builder::from_default_env().init();
 
-    let orkut_graph = read_orkutdir(Path::new(ORKUT_DATA_DIR));
+    let orkut_graph = read_orkut_graph(Path::new(ORKUT_DATA_DIR));
     if orkut_graph.is_err() {
         println!("cannot load orkut graph");
     }
@@ -63,4 +105,5 @@ pub fn main() {
         log::info!("orkhut : points of block : {blocnum} , {bsize}");
     }
     //
+    let communities = read_orkut_com(Path::new(ORKUT_DATA_DIR)).unwrap();
 } // end of main
